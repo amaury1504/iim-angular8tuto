aeb804b8ba6ae04a64e7e7af3252778791b66eef
amaury1504 <amaury.gimonnet@devinci.fr>
2019-09-20 13:39:58 +0200

    etape17

--- src/app/product-get/product-get.component.ts
+++ src/app/product-get/product-get.component.ts
@@ -21,5 +21,9 @@ export class ProductGetComponent implements OnInit {
         this.products = data;
     });
   }
-
+  deleteProduct(id) {
+    this.ps.deleteProduct(id).subscribe(res => {
+      this.products.splice(id, 1);
+    });
+}
 }
